6651cd3ba740ac559c92d0159642be28e856fdbd
AineMota <motaaine1@gmail.com>
2021-10-21 00:16:28 -0300

    iniciei a funcao forEach

---
+++ java7.js
@@ -0,0 +1,18 @@
+//GLOBAL
+function loopingFor(limite, expression){
+    for(let i = 0; i < limite; i++){
+        expression
+    }
+}
+
+
+//forEach
+const forEachObj ={
+    intencao : 'Retornar todos os números pares contidos na array', 
+    
+    array : [2, 3, 4, 5, 2, 9, 4, 23, 56,],
+
+    iteracao : 
+}
+//-criar a funca callback
+//-criar  o looping que executara a funcao callback(REUTILIZAR A MESMA FUNCAO ????????)
